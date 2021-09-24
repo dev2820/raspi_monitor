b01491e4f48564be28a981fc2168452d09a7a5e0
dev2820 <second28200@gmail.com>
2021-09-24 14:49:26 +0900

    간단한 파일 출력 코드 작성

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 'use strict';
 
-const fs = require('fs')
+const fs = require('fs/promises')
+const path = require('path')
 
 const intervalSecond = process.env.INTERVAL*1000;
 
-fs.readFile()
+const filePathList = [
+    "/proc/uptime",      // uptime 정보
+    "/proc/loadavg",     // loadavg 1분 5분 15분 정보
+    "/var/run/utmp",     // 현재 접속중인 유저 저장
+    "/proc/stat",        // cpu 사용률 정보
+    "/proc/meminfo",     // 메모리 정보
+    "/proc/diskstats",   // 디스크 정보
+    "/proc/net/dev"      // 네트워크 정보
+];
+
+const filePromiseList = filePathList.map(path=>{
+    return fs.readFile(path,{ encoding:'utf-8'})
+})
+
+Promise.all(filePromiseList).then(contents=>{
+    console.log(contents);
+})
+
+
