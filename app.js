26dd277a4cfb60d38917c6274baffef19c37b190
dev2820 <second28200@gmail.com>
2021-09-28 15:59:57 +0900

    countList 디버깅중

--- app.js
+++ app.js
@@ -699,8 +699,8 @@ const mainLoop = async (interval,objs,dbOptions) => {
             
             const countList = await Promise.all(promiseList);
             promiseList.splice(0);
-            console.log(countList)
-            let cnt = countList[0];
+            console.log(countList[0][0])
+            let {cnt} = countList[0][0];
             if(cnt>300) {
                 const deleteLatest300Query = `DELETE FROM cpu_status ORDER BY date limit ${cnt-300}`;
                 promiseList.push(connectionList[0].query(deleteLatest300Query));
